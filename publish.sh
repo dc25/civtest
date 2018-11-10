1f0052c20052d6bf8902ac3e89aef3c2b23fec0a
Dave Compton <davecompton7@gmail.com>
2018-11-10 17:06:27 +0000

    gh-pages commit

---
+++ publish.sh
@@ -0,0 +1,12 @@
+  rm -rf _site
+  jekyll build
+  git stash
+  git branch -D gh-pages
+  git checkout --orphan gh-pages
+  git rm -rf .
+  git --work-tree _site/ add .
+  git --work-tree _site commit -a -m 'gh-pages commit'
+  git --work-tree _site push -f origin gh-pages
+  git checkout master
+  git checkout .
+  git stash pop
